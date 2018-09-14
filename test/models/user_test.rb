d0f48e6033ae78f96131954377e1f34ccf8e289c
Part1nax777 <serewka-ru@yandex.ru>
2018-09-14 13:16:08 +0300

    add test for correct email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,21 @@ class UserTest < ActiveSupport::TestCase
 	@user.email = "a" * 244 + "@example.com"	
 	assert_not @user.valid?
 	end
+
+	test "email validation should accept valid addresses" do
+	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+	valid_addresses.each do |valid_address|
+		@user.email = valid_address
+		assert @user.email?, "#{valid_address.inspect} should be valid"
+		end
+	end
+
+	test "email validation should reject valid addresses" do
+	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+	invalid_addresses.each do |invalid_addresses|
+		@user.email = invalid_addresses
+		assert_not @user.valid?, "#{invalid_addresses.inspect} should be invalid"
+		end
+	end
+	
 end
