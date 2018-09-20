96dc4ff5abdad6dd40054f2e5af71b0076fe8a80
Part1nax777 <serewka-ru@yandex.ru>
2018-09-20 13:44:20 +0300

    test enter after autorization

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  def setup
+	@user = users(:serewka)
+  end
+
   test "login with invalid information" do
 	get login_path #переход на страницу login
 	assert_template 'sessions/new' #страница отображается
@@ -10,4 +15,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 	get root_path #переход на главную страницу
 	assert flash.empty? #выводится предупреждение о ошибке
   end
+
+  test "login with valid information" do
+	get login_path
+	post login_path, params: { session: { email: @user.email, password: 'password' } }
+	assert_redirected_to @user
+	follow_redirect!
+	assert_template 'users/show'
+	assert_select "a[href=?]", login_path, count: 0
+	assert_select "a[href=?]", logout_path
+	assert_select "a[href=?]", user_path(@user)
+  end
+
 end
