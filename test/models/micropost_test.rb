f03f089ad7f741e00494f09b10c6b3ae632ac525
Part1nax777 <serewka-ru@yandex.ru>
2018-10-01 23:08:47 +0300

    add length micropost 140 chars

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -16,5 +16,10 @@ class MicropostTest < ActiveSupport::TestCase
 	@micropost.user_id = nil
 	assert_not @micropost.valid?
   end
+
+  test "content should be present" do
+	@micropost.content = "a" * 141
+	assert_not @micropost.valid?
+  end
 	
 end
