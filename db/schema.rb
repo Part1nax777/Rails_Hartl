b82b9b29c29bedf5cb9702c74b06136fca6f34ea
Part1nax777 <serewka-ru@yandex.ru>
2018-10-02 20:29:34 +0300

    add microposts for 6 users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180926193433) do
+ActiveRecord::Schema.define(version: 20181001191921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,6 +35,9 @@ ActiveRecord::Schema.define(version: 20180926193433) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
+  add_foreign_key "microposts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@ User.create!(name: "Serewka", email: "spsserega@gmail.com", password: "1234567",
   User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
 
+users = User.order(:created_at).take(6)
+50.times do 
+	content = Faker::Lorem.sentence(5)
+	users.each { |user| user.microposts.create!(content: content) }
+end
