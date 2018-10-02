2f2eea6d2c954d5b9ae6a43741280705922864cf
Part1nax777 <serewka-ru@yandex.ru>
2018-10-02 12:57:39 +0300

    micropost.build

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -5,7 +5,7 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
 	@user = users(:serewka)
 	#not correct
-	@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+	@micropost = @user.microposts.build(content: "Lorem ipsum", user_id: @user.id)
   end
 
   test "should be valid" do
