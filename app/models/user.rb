41109d2f1f1bf9beece707cf8052cefcd8c75a2c
Part1nax777 <serewka-ru@yandex.ru>
2018-10-05 12:30:39 +0300

    model for follow/unfollow end

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
 
 	has_many :microposts, dependent: :destroy
 	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+
 	attr_accessor :remember_token, :activation_token, :reset_token
 	
 	before_save { self.email = email.downcase }
@@ -80,12 +83,12 @@ class User < ApplicationRecord
 
 	# Выполняет подписку на сообщения пользователя
 	def follow(other_user)
-	active_relationships.create(followed_id: other_user.id)
+	following << other_user
 	end
 
 	# Отменяет подписку на сообщения пользователя
 	def unfollow(other_user)
-	active_relationships.find_by(followed_id: other_user.id).destroy
+	following.delete(other_user)
 	end
 
 	# Возвращает true если текущий пользователь читает другого пользователя
