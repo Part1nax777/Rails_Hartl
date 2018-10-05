7eb02d6ccc11d17fe32076cd0c2924e3b28ccf39
Part1nax777 <serewka-ru@yandex.ru>
2018-10-05 12:07:43 +0300

    tests for follow/unfollow/following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
 
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
 	attr_accessor :remember_token, :activation_token, :reset_token
 	
 	before_save { self.email = email.downcase }
@@ -76,7 +78,22 @@ class User < ApplicationRecord
 	Micropost.where("user_id = ?", id)
 	end
 
-private
+	# Выполняет подписку на сообщения пользователя
+	def follow(other_user)
+	active_relationships.create(followed_id: other_user.id)
+	end
+
+	# Отменяет подписку на сообщения пользователя
+	def unfollow(other_user)
+	active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	# Возвращает true если текущий пользователь читает другого пользователя
+	def following?(other_user)
+	following.include?(other_user)
+	end
+
+private    
 
 	# Преобразует адрес электронной почты в нижний регистр
 	def downcase_email
